268bd4a8e0ca2f3320570d4c75f80f5412cf30a5
Tim Phillips <tim.p.phillips@gmail.com>
2020-03-22 21:59:07 -0700

    add Navbar, global styles, and head

---
+++ pages/_app.js
@@ -0,0 +1,24 @@
+/* eslint-disable react/prop-types */
+
+import React from 'react'
+import Head from 'next/head'
+
+import Navbar from '../components/Navbar'
+
+import '../css/style.css'
+
+const App = ({ Component, pageProps }) => {
+  return (
+    <>
+      <Head>
+        <title>KCHUNG Radio 1630AM</title>
+        <link rel="icon" type="image/png" href="/img/favicon.ico" />
+        <meta property="og:image" content="/img/kchungforfacebook.jpg" />
+      </Head>
+      <Navbar />
+      <Component {...pageProps} />
+    </>
+  )
+}
+
+export default App
