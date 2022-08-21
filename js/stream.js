371f708ee3766b04b390787210eb8b72c2bcc629
Tim Phillips <3578574+tim-phillips@users.noreply.github.com>
2022-08-21 10:23:41 -0700

    fix stream metadata (#45)

--- js/stream.js
+++ js/stream.js
@@ -1,32 +1,32 @@
 ;(function() {
   var liveInfoUrl = 'https://kchungradio.airtime.pro/api/live-info-v2'
-  var trackNameEl = document.getElementById('trackName')
+
   var showNameEl = document.getElementById('showName')
+  var trackNameEl = document.getElementById('trackName')
 
   // this runs after the DOM has loaded
   $(function() {
     $.get(liveInfoUrl, function(liveInfo) {
-      var trackNameText
       var showNameText
+      var trackNameText
 
       if (
         liveInfo &&
         liveInfo.tracks &&
         liveInfo.tracks.current &&
-        liveInfo.tracks.current.metadata
+        liveInfo.tracks.current.metadata &&
+        liveInfo.tracks.current.metadata.filepath
       ) {
         trackNameText = liveInfo.tracks.current.metadata.filepath
-      }
-
-      if (liveInfo && liveInfo.shows && liveInfo.shows.current) {
-        showNameText = liveInfo.shows.current.name
-      }
-
-      if (trackNameText && showNameText) {
         trackNameEl.innerText = trackNameText
+      } else if (
+        liveInfo &&
+        liveInfo.shows &&
+        liveInfo.shows.current &&
+        liveInfo.shows.current.name
+      ) {
+        showNameText = liveInfo.shows.current.name
         showNameEl.innerText = showNameText
-      } else {
-        showNameEl.innerText = 'kchung radio is offline...'
       }
     })
   })
