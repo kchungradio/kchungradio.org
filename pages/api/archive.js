f84ae75cf3fd7cfafb855b1fe9c74a5cd39f2af7
Tim Phillips <timphillips@hey.com>
2022-08-31 16:53:02 -0700

    refactor archive api

--- pages/api/archive.js
+++ pages/api/archive.js
@@ -7,23 +7,20 @@ export default async function handler(req, res) {
 
   // console.log({ limit, page, search })
 
-  const _search = search.trim().replace(/ /g, '%').replace(/\*/g, '%')
+  const searchWild = search.trim().replace(/ /g, '%').replace(/\*/g, '%')
 
   const data = await query(
-    `SELECT date, path FROM radio.archive
+    `SELECT date, path
+     FROM radio.archive
      WHERE path ILIKE $3
      ORDER BY date DESC
      LIMIT $1 OFFSET $2`,
-    [limit, page * limit, `%${_search}%`]
+    [limit, page * limit, `%${searchWild}%`]
   )
-    .then((data) => data.rows)
-    .then((rows) =>
-      rows.map((row) => ({
-        ...row,
-        date: row.date.toISOString().split('T')[0],
-      }))
-    )
-    .catch(console.error)
+  const rows = data.rows.map((row) => ({
+    ...row,
+    date: row.date.toISOString().split('T')[0],
+  }))
 
-  res.status(200).json(data)
+  res.status(200).json(rows)
 }
