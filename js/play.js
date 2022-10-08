30504ee7675e935440fc6e42a231ba3bcd95e9f0
JoshBurke <joshDgreat37@gmail.com>
2022-10-08 02:34:25 -0700

    add javascript for handling playing/pausing/metadata

--- js/play.js
+++ js/play.js
@@ -1,23 +1,65 @@
 var audio = document.getElementById('player')
+var audioPublic = document.getElementById('player-public')
 
 var playButton = $('.play-button')
 var pauseButton = $('.pause-button')
+var playButtonPublic = $('.play-button-public')
+var pauseButtonPublic = $('.pause-button-public')
 
 playButton.click(function(event) {
   event.preventDefault()
-
-  playButton.toggleClass('hidden')
-  pauseButton.toggleClass('hidden')
-
-  audio.volume = 1.0
-  audio.play()
+  play()
 })
 
 pauseButton.click(function(event) {
   event.preventDefault()
+  pause()
+})
 
+playButtonPublic.click(function(event) {
+  event.preventDefault()
+  playPublic()
+})
+
+pauseButtonPublic.click(function(event) {
+  event.preventDefault()
+  pausePublic()
+})
+
+function play() {
+  playButton.toggleClass('hidden')
+  pauseButton.toggleClass('hidden')
+
+  if (playButtonPublic.hasClass('hidden')) {
+    pausePublic()
+  }
+
+  audio.volume = 1.0
+  audio.play()
+}
+
+function pause() {
   playButton.toggleClass('hidden')
   pauseButton.toggleClass('hidden')
 
   audio.pause()
-})
+}
+
+function playPublic() {
+  playButtonPublic.toggleClass('hidden')
+  pauseButtonPublic.toggleClass('hidden')
+
+  if (playButton.hasClass('hidden')) {
+    pause()
+  }
+
+  audioPublic.volume = 1.0
+  audioPublic.play()
+}
+
+function pausePublic() {
+  playButtonPublic.toggleClass('hidden')
+  pauseButtonPublic.toggleClass('hidden')
+
+  audioPublic.pause()
+}
--- js/stream.js
+++ js/stream.js
@@ -1,33 +1,60 @@
 ;(function() {
   var liveInfoUrl = 'https://kchungradio.airtime.pro/api/live-info-v2'
+  var publicInfoUrl = 'http://s9.voscast.com:7376/7.html'
 
   var showNameEl = document.getElementById('showName')
   var trackNameEl = document.getElementById('trackName')
 
+  var playButton = $('.play-button')
+  var playButtonPublic = $('.play-button-public')
+
+  var contentRegex = /<body>(.*)<\/body>/
+
+  function parseShoutcastResponse(html) {
+    var content = html.match(contentRegex)[1]
+    var parts = content.split(',')
+    if (parts.length < 7 || !parts[6]) {
+      return null
+    }
+    return parts[6]
+  }
+
   // this runs after the DOM has loaded
   $(function() {
-    $.get(liveInfoUrl, function(liveInfo) {
-      var showNameText
-      var trackNameText
+    if (!playButton.hasClass('hidden')) {
+      // If radio stream is playing
+      $.get(liveInfoUrl, function(liveInfo) {
+        var showNameText
+        var trackNameText
 
-      if (
-        liveInfo &&
-        liveInfo.tracks &&
-        liveInfo.tracks.current &&
-        liveInfo.tracks.current.metadata &&
-        liveInfo.tracks.current.metadata.filepath
-      ) {
-        trackNameText = liveInfo.tracks.current.metadata.filepath
-        trackNameEl.innerText = trackNameText
-      } else if (
-        liveInfo &&
-        liveInfo.shows &&
-        liveInfo.shows.current &&
-        liveInfo.shows.current.name
-      ) {
-        showNameText = liveInfo.shows.current.name
-        showNameEl.innerText = showNameText
-      }
-    })
+        if (
+          liveInfo &&
+          liveInfo.tracks &&
+          liveInfo.tracks.current &&
+          liveInfo.tracks.current.metadata &&
+          liveInfo.tracks.current.metadata.filepath
+        ) {
+          trackNameText = liveInfo.tracks.current.metadata.filepath
+          trackNameEl.innerText = trackNameText
+        } else if (
+          liveInfo &&
+          liveInfo.shows &&
+          liveInfo.shows.current &&
+          liveInfo.shows.current.name
+        ) {
+          showNameText = liveInfo.shows.current.name
+          showNameEl.innerText = showNameText
+        }
+      })
+    } else if (!playButtonPublic.hasClass('hidden')) {
+      // If public stream is playing
+      $.get(publicInfoUrl, function(publicInfoHtml) {
+        var showNameText = parseShoutcastResponse(publicInfoHtml)
+        if (showNameText) {
+          trackNameEl.innerText = null
+          showNameEl.innerText = showNameText
+        }
+      })
+    }
   })
 })()
