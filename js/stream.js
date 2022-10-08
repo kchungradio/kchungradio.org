f63247db6962c1efc53ba85197918d6d92be52d7
Josh Burke <me@joshburke.xyz>
2022-10-08 14:59:24 -0700

    Add voscast stream (#46)
    * add new stream play button to html + css
    
    * add javascript for handling playing/pausing/metadata
    
    * package-lock and gitignore
    
    * default metadata to null if nothing's playing
    
    * revert last commit
    
    * add comment
    
    * change button labels
    
    * add metadata for public stream hardcoded
    
    * reset text for show/track when changed
    
    * add disclaimer about browser

--- js/stream.js
+++ js/stream.js
@@ -1,11 +1,18 @@
 ;(function() {
   var liveInfoUrl = 'https://kchungradio.airtime.pro/api/live-info-v2'
+  // Visit URL below to understand SHOUTcast's metadata html response format
+  // TODO: Figure out how to pull this info without getting a CORS error
+  // var publicInfoUrl = 'http://s9.voscast.com:7376/7.html'
 
   var showNameEl = document.getElementById('showName')
   var trackNameEl = document.getElementById('trackName')
 
-  // this runs after the DOM has loaded
-  $(function() {
+  var playButton = $('.play-button')
+  var playButtonPublic = $('.play-button-public')
+
+  playButton.click(function(event) {
+    event.preventDefault()
+    // If radio stream is playing
     $.get(liveInfoUrl, function(liveInfo) {
       var showNameText
       var trackNameText
@@ -19,6 +26,7 @@
       ) {
         trackNameText = liveInfo.tracks.current.metadata.filepath
         trackNameEl.innerText = trackNameText
+        showNameEl.innerText = ''
       } else if (
         liveInfo &&
         liveInfo.shows &&
@@ -27,7 +35,17 @@
       ) {
         showNameText = liveInfo.shows.current.name
         showNameEl.innerText = showNameText
+        trackNameEl.innerText = ''
       }
     })
   })
+
+  playButtonPublic.click(function(event) {
+    event.preventDefault()
+    showNameEl.innerText = 'Live from the Geffen Contemporary MOCA'
+    trackNameEl.innerText = ''
+  })
+
+  // this runs after the DOM has loaded
+  // $(function() {})
 })()
