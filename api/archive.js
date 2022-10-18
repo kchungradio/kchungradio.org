150892dadc7201ccd50774629283e672f7710d38
Tim Phillips <timphillips@hey.com>
2022-10-18 08:58:01 -0700

    run prettier on api

--- api/archive.js
+++ api/archive.js
@@ -1,16 +1,13 @@
 const db = require('./lib/db')
 
-module.exports = async function(req, res) {
+module.exports = async function (req, res) {
   if (req.url.includes('favicon')) return
 
   const { limit = 50, page = 0, search = '' } = req.query
 
   // console.log({ limit, page, search })
 
-  const _search = search
-    .trim()
-    .replace(/ /g, '%')
-    .replace(/\*/g, '%')
+  const _search = search.trim().replace(/ /g, '%').replace(/\*/g, '%')
 
   const data = await db
     .query(
@@ -20,11 +17,11 @@ module.exports = async function(req, res) {
        LIMIT $1 OFFSET $2`,
       [limit, page * limit, `%${_search}%`]
     )
-    .then(data => data.rows)
-    .then(rows =>
-      rows.map(row => ({
+    .then((data) => data.rows)
+    .then((rows) =>
+      rows.map((row) => ({
         ...row,
-        date: row.date.toISOString().split('T')[0]
+        date: row.date.toISOString().split('T')[0],
       }))
     )
     .catch(console.error)
