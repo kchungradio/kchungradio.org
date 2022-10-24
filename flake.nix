6d4be529f23914b152fada3b93f0ae097df98714
solomon <ssbothwell@gmail.com>
2022-10-24 00:07:00 -0700

    Updates README

--- flake.nix
+++ flake.nix
@@ -9,17 +9,7 @@
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
-      nodePackages = import ./default.nix {
-        inherit pkgs system;
-        nodejs = pkgs."nodejs-16_x";
-      };
     in {
-      defaultPackage.x86_64-linux = nodePackages."kchungradio.org";
-
-      overlay = final: prev: {
-        graphqurl = nodePackages."kchungradio.org" ;
-      };
-
       devShell.x86_64-linux = pkgs.mkShell {
         buildInputs = [
           pkgs.nodePackages.prettier
