2088a746ef9ddfc5fff144eae0a6f45c6e26b2fb
solomon <ssbothwell@gmail.com>
2022-10-24 23:50:07 -0700

    Adjustments to Stream Page

--- js/stream.js
+++ js/stream.js
@@ -10,34 +10,30 @@
   var playButton = $('.play-button')
   var playButtonPublic = $('.play-button-public')
 
-  playButton.click(function(event) {
-    event.preventDefault()
-    // If radio stream is playing
-    $.get(liveInfoUrl, function(liveInfo) {
-      var showNameText
-      var trackNameText
+  $.get(liveInfoUrl, function(liveInfo) {
+    var showNameText
+    var trackNameText
 
-      if (
-        liveInfo &&
-        liveInfo.tracks &&
-        liveInfo.tracks.current &&
-        liveInfo.tracks.current.metadata &&
-        liveInfo.tracks.current.metadata.filepath
-      ) {
-        trackNameText = liveInfo.tracks.current.metadata.filepath
-        trackNameEl.innerText = trackNameText
-        showNameEl.innerText = ''
-      } else if (
-        liveInfo &&
-        liveInfo.shows &&
-        liveInfo.shows.current &&
-        liveInfo.shows.current.name
-      ) {
-        showNameText = liveInfo.shows.current.name
-        showNameEl.innerText = showNameText
-        trackNameEl.innerText = ''
-      }
-    })
+    if (
+      liveInfo &&
+      liveInfo.tracks &&
+      liveInfo.tracks.current &&
+      liveInfo.tracks.current.metadata &&
+      liveInfo.tracks.current.metadata.filepath
+    ) {
+      trackNameText = liveInfo.tracks.current.metadata.filepath
+      trackNameEl.innerText = trackNameText
+      showNameEl.innerText = ''
+    } else if (
+      liveInfo &&
+      liveInfo.shows &&
+      liveInfo.shows.current &&
+      liveInfo.shows.current.name
+    ) {
+      showNameText = liveInfo.shows.current.name
+      showNameEl.innerText = showNameText
+      trackNameEl.innerText = ''
+    }
   })
 
   playButtonPublic.click(function(event) {
