5c6a1f8a80063b6f06fe5a4058f1c20413e7de83
Solomon <ssbothwell@gmail.com>
2022-10-29 13:26:11 -0700

    Responsive Layout (#58)
    * Responsive Navbar
    
    * Responsive Stream Player
    
    * Responsive Main Viewport
    
    * Add meta viewport tag for mobile
    
    * Restyle chat widget.

--- js/stream.js
+++ js/stream.js
@@ -10,42 +10,31 @@
   var playButton = $('.play-button')
   var playButtonPublic = $('.play-button-public')
 
-  playButton.click(function(event) {
-    event.preventDefault()
-    // If radio stream is playing
-    $.get(liveInfoUrl, function(liveInfo) {
-      var showNameText
-      var trackNameText
+  $.get(liveInfoUrl, function(liveInfo) {
+    var showNameText
+    var trackNameText
 
-      if (
-        liveInfo &&
-        liveInfo.tracks &&
-        liveInfo.tracks.current &&
-        liveInfo.tracks.current.metadata &&
-        liveInfo.tracks.current.metadata.filepath
-      ) {
-        trackNameText = liveInfo.tracks.current.metadata.filepath
-        trackNameEl.innerText = trackNameText
-        showNameEl.innerText = ''
-      } else if (
-        liveInfo &&
-        liveInfo.shows &&
-        liveInfo.shows.current &&
-        liveInfo.shows.current.name
-      ) {
-        showNameText = liveInfo.shows.current.name
-        showNameEl.innerText = showNameText
-        trackNameEl.innerText = ''
-      }
-    })
+    if (
+      liveInfo &&
+      liveInfo.tracks &&
+      liveInfo.tracks.current &&
+      liveInfo.tracks.current.metadata &&
+      liveInfo.tracks.current.metadata.filepath
+    ) {
+      trackNameText = liveInfo.tracks.current.metadata.filepath
+      trackNameEl.innerText = trackNameText
+      showNameEl.innerText = ''
+    } else if (
+      liveInfo &&
+      liveInfo.shows &&
+      liveInfo.shows.current &&
+      liveInfo.shows.current.name
+    ) {
+      showNameText = liveInfo.shows.current.name
+      showNameEl.innerText = showNameText
+      trackNameEl.innerText = ''
+    }
   })
-
-  playButtonPublic.click(function(event) {
-    event.preventDefault()
-    showNameEl.innerText = 'Live from the Geffen Contemporary MOCA'
-    trackNameEl.innerText = ''
-  })
-
   // this runs after the DOM has loaded
   // $(function() {})
 })()
