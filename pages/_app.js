32d114ca4604b66c1cf72e81baed760b79c1b1fc
Tim Phillips <timphillips@hey.com>
2022-11-02 12:04:22 -0700

    remove redundant css

--- pages/_app.js
+++ pages/_app.js
@@ -18,10 +18,6 @@ function App({ Component, pageProps }) {
       <Head>
         <title>KCHUNG Radio 1630AM</title>
         <link rel="icon" type="image/png" href="/img/favicon.ico" />
-        <link
-          rel="stylesheet"
-          href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css"
-        />
         <meta property="og:image" content="/img/kchungforfacebook.jpg" />
       </Head>
       <Navbar />
