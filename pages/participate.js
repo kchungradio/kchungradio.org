25dbe59b0615bc63562a8976178130adc7a86ed5
solomon <ssbothwell@gmail.com>
2023-05-09 13:19:01 -0700

    Updates participation description and monthly donation amount.

--- pages/participate.js
+++ pages/participate.js
@@ -1,38 +1,12 @@
 import React from 'react'
+import Link from 'next/link'
 
 function ParticipatePage() {
   return (
     <div id="main">
       <h3>participate</h3>
       <p>anyone can be involved.</p>
-      <p>
-        If you would like to have a show or are interested in helping out around
-        the station, please fill out the{' '}
-        <a href="https://docs.google.com/a/kchungradio.org/spreadsheet/viewform?formkey=dGNfVEt1N00weDh2bk14QTFNTl9NSnc6MQ#gid=0">
-          kchung radio volunteer application
-        </a>{' '}
-        form. once you submit the form, you will be placed on the waiting list
-        to get a monthly show in our regular broadcast schedule. when
-        fortnightly shows become available, they are offered to our current
-        monthly djs. if you would like a weekly show, you must also volunteer as
-        a station manager.
-      </p>
-      <p>
-        you can sign up for the sublist through the{' '}
-        <a href="https://docs.google.com/a/kchungradio.org/spreadsheet/viewform?formkey=dGNfVEt1N00weDh2bk14QTFNTl9NSnc6MQ#gid=0">
-          volunteer form
-        </a>
-        . when one of our regular djs cannot make their show, we call upon our
-        sublist to fill in.
-      </p>
-      <p>
-        if you would like to do a one-off show or a short series of up to three
-        shows, send a proposal to{' '}
-        <a href="mailto:liveinstudios@kchungradio.org">
-          liveinstudios@kchungradio.org
-        </a>
-        .
-      </p>
+      <p>KCHUNG is a community non-profit that runs on volunteers, member dues, donations, and grants. if you are interested in having a radio show or helping out around the station, please email <a href="mailto:generalmanager@kchungradio.org">generalmanager@kchungradio.org</a>. kchung dj dues are $12 a month and can be set up as a <Link href="/donate">recurring donation</Link> via paypal.</p>
       <br />
       <h3>where to send your music</h3>
       <p>send your cds, tapes, records, and wax cylinders to:</p>
