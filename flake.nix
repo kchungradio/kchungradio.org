820028192f18de4e971d0bf5de9fc23be76f10a6
solomon <ssbothwell@gmail.com>
2023-12-11 22:01:15 -0800

    Nix Build

--- flake.nix
+++ flake.nix
@@ -3,20 +3,38 @@
 
   inputs = {
     nixpkgs.url = github:nixos/nixpkgs/nixos-23.11;
+    flake-utils.url = github:numtide/flake-utils;
   };
 
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, flake-utils }:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
-    in {
-      devShell.x86_64-linux = pkgs.mkShell {
-        buildInputs = [
-          pkgs.nodePackages.prettier
-          pkgs.nodePackages.vercel
-          pkgs.nodejs
-          pkgs.yarn
-        ];
-      };
-    };
+    in
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let pkgs = import nixpkgs { inherit system; };
+        in rec {
+          devShells.default = pkgs.mkShell {
+            buildInputs = [
+              pkgs.nodePackages.prettier
+              pkgs.nodePackages.vercel
+              pkgs.nodejs
+              pkgs.yarn
+            ];
+          };
+
+          formatter = pkgs.nixpkgs-fmt;
+
+          packages.default = pkgs.buildNpmPackage rec {
+            pname = "kchungradio.org";
+            version = "0.1.0.0";
+            buildInputs = [
+              pkgs.nodejs
+            ];
+            src = ./.;
+            npmDepsHash = "sha256-ej6qnzSAkEOBNOYwPRaJWmst7BQRe3BDdbi76y708Aw=";
+            npmBuild = "npm run build";
+          };
+        });
 }
