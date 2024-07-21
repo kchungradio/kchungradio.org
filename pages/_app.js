c5a8c3ffef160b1d0831e038a2dee09abc8ba4fb
birthdaycorp <matthew.r.keable@gmail.com>
2024-07-21 11:39:04 -0700

    add ga4 and heatmap

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,7 @@ import Head from 'next/head'
 import { config } from '@fortawesome/fontawesome-svg-core'
 import { PayPalScriptProvider } from '@paypal/react-paypal-js'
 import { paypalConfig } from '../lib/paypal'
+import Script from 'next/script'
 
 import Navbar from '../components/Navbar'
 import Stream from '../components/Stream'
@@ -27,6 +28,27 @@ function App({ Component, pageProps }) {
       <PayPalScriptProvider deferLoading options={paypalConfig}>
         <Component {...pageProps} />
       </PayPalScriptProvider>
+
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-79TEET1DEV" strategy="beforeInteractive" />
+      
+      <Script strategy="beforeInteractive" >
+        {
+          `window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-79TEET1DEV');`
+        }
+        </Script>
+        <Script strategy="beforeInteractive" >
+        {
+          `(function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "n90784i96d");`
+        }
+        </Script>
     </>
   )
 }
