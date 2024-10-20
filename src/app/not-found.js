1debec966051b9ed47a74b5c52fc048fdc257d3d
Tim Phillips <3578574+tim-phillips@users.noreply.github.com>
2024-10-20 16:18:07 -0700

    add 404 not found page

---
+++ src/app/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <div id="main">
+      <p>{`that page doesn't exist`}</p>
+      <p>{`maybe you want to listen to something or check the archive`}</p>
+    </div>
+  )
+}
