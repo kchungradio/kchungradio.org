f8aa3dea8d246921525a6b1669a095b2220eba0a
Brock <brock@warpmail.net>
2026-05-10 16:10:11 -0700

    new chat

--- src/app/stream/page.js
+++ src/app/stream/page.js
@@ -1,13 +1,17 @@
 function Chatbox() {
   return (
     <div className="chatbox">
-      <iframe
-        src="https://embed.tlk.io/kchung?custom_css_path=https://www.kchungradio.org/css/chat-style.css&amp;theme=theme--minimal"
-        width="100%"
-        height="100%"
-        frameBorder="0"
-        style={{ height: '400px' }}
-      ></iframe>
+      
+<iframe
+  title="KCHUNG Radio chat room"
+  src="https://app.radiocult.fm/embed/chat/kchung-radio-01e54a81?theme=custom&primaryColor=%23ffffff&corners=sharp&playerDisplay=metadata&ptc=%23000000&stc=%23000000&bc=%23ff0000&font=Courier%2BPrime&inmc=%235de2ff&outmc=%23ff98f2&stationmc=%238dff57&sepc=%231f3045"
+  width="100%"
+  height="600px"
+  scrolling="no"
+  frameborder="0"
+  seamless
+  allowtransparency="true"
+></iframe>
     </div>
   )
 }
